fc6b8a0e0fc607f6bc977f32421f23360fbec3f1
Matthew Cross <matthew.evan.cross@gmail.com>
2016-07-23 14:32:32 +1000

    Add code to control motors

---
+++ index.js
@@ -0,0 +1,29 @@
+// Uses proximity sensor on the front to tell the distance from the object
+// Assumes prox sensor is plugged into port 2 - if not change the pin to use
+// the pin for appropriate port and the second pin within it.
+
+var five = require("johnny-five");
+var board = new five.Board();
+var max_speed_l = 150;
+var max_speed_r = 140;
+
+board.on("ready", function () {
+
+  var proximity = new five.Proximity({
+    freq: 1000,
+    controller: "HCSR04",
+    pin: 10
+  });
+
+  var l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+  var r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+  proximity.on("data", function () {
+
+    if (this.cm < 30) {
+      l_motor.reverse(max_speed_l);
+      r_motor.forward(max_speed_r);
+    }
+
+  });
+});
